6e53acc312be82143000a029503f45577eb9d359
Lucas Correia <lucascorreiamelo12@gmail.com>
2026-04-03 17:44:13 -0300

    Add files via upload

--- Estudando JavaScript/OlaMundo.js
+++ Estudando JavaScript/OlaMundo.js
@@ -1,1 +1,9 @@
 console.log("Olá, Mundo!");
+
+// Para exibir uma mensagem na tela em JavaScript, você pode usar a função console.log(). Esta função é comumente utilizada para imprimir mensagens no console do navegador ou do ambiente de desenvolvimento. No exemplo acima, a mensagem "Olá, Mundo!" será exibida no console quando o código for executado.
+
+// E também é possível exibir a mensagem em uma janela de alerta usando a função alert():
+
+alert("Olá, Mundo!");
+
+// Neste caso, a mensagem "Olá, Mundo!" será exibida em uma janela de alerta no navegador. A função alert() é útil para chamar a atenção do usuário ou fornecer informações importantes. No entanto, é importante usar alertas com moderação, pois eles podem interromper a experiência do usuário se usados em excesso.
